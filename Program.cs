900c2869fa41e6a09eb0da7049799a6834d3f096
Ernest <emudjambere@gmail.com>
2025-07-06 20:05:04 -0400

    Apply EF Core migrations on startup

--- Program.cs
+++ Program.cs
@@ -72,6 +72,9 @@ app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+
     var seeder = scope.ServiceProvider.GetRequiredService<IRoleSeeder>();
     await seeder.SeedRolesAsync();
 }
