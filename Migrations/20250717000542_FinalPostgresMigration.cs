2586dab01eec1eec51504e9afd76c9b9204fe9b4
Ernest <emudjambere@gmail.com>
2025-07-16 20:07:12 -0400

    Add FinalPostgresMigration for production PostgreSQL

---
+++ Migrations/20250717000542_FinalPostgresMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sanalink.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalPostgresMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
